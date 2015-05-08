9efe68f31185638bbca306b970b5368d3088a0ad
Damian Pyc <pycu0007@gmail.com>
2015-05-08 10:36:39 +0200

    pole trapezu: implementacja metod

--- spec/dpyc/Tools/PoleTSpec.php
+++ spec/dpyc/Tools/PoleTSpec.php
@@ -11,4 +11,16 @@ class PoleTSpec extends ObjectBehavior
     {
         $this->shouldHaveType('dpyc\Tools\PoleT');
     }
+   
+   function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(455)->getH()->shouldReturn(455);
+    }
+    
+    function it_should_calculate_field_trapeze()
+    {
+    	$this->setA(2)->setB(2)->setH(2)->trapeze()->shouldReturn(4);
+    }
 }
