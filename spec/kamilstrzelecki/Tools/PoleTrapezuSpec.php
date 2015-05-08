b04afabd7bcff341f17640c8487cf2c8a77d447a
Kamil Strzelecki <kstrzelecki@outlook.com>
2015-05-08 09:44:32 +0200

    kamilstrzelecki/pole trapezu: getery i setery

--- spec/kamilstrzelecki/Tools/PoleTrapezuSpec.php
+++ spec/kamilstrzelecki/Tools/PoleTrapezuSpec.php
@@ -11,4 +11,16 @@ class PoleTrapezuSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamilstrzelecki\Tools\PoleTrapezu');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setC(324)->getC()->shouldReturn(324);
+    }
+    
+    function it_should_calculate_area()
+    {
+       $this->setA(2)->setB(3)->setC(2)->area()->shouldReturn(5);
+    }
 }
