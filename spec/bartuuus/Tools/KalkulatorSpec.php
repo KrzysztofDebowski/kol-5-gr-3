abaa0d29a1da2ca18daa56ad63b7f1117dd8476d
Bartlomiej Paziewski <bartus.paziewski@gmail.com>
2015-05-08 10:00:44 +0200

    bartuuus/poleTrapezu: implementacja metod

--- spec/bartuuus/Tools/KalkulatorSpec.php
+++ spec/bartuuus/Tools/KalkulatorSpec.php
@@ -11,4 +11,16 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('bartuuus\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(555)->getA()->shouldReturn(555);
+        $this->setB(100)->getB()->shouldReturn(100);
+        $this->setH(345)->getH()->shouldReturn(345);
+    }
+    
+    function it_should_calculate_trapez()
+    {
+        $this->setA(2.0)->setB(3.0)->setH(2.0)->sum()->shouldReturn(5.0);
+    }
 }
--- src/bartuuus/Tools/Kalkulator.php
+++ src/bartuuus/Tools/Kalkulator.php
@@ -4,4 +4,49 @@ namespace bartuuus\Tools;
 
 class Kalkulator
 {
+    
+    private $a;
+    private $b;
+    private $h;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+         return $this->a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function setH($h)
+    {
+        $this->h = $h;
+
+        return $this;
+    }
+
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function sum()
+    {
+        return $this->a + $this->b * $this->h * 0.5;
+    }
 }
