9bcb60e422938953235e9cb0157aa65f78c0aa7b
Gojira <gojira.green@gmail.com>
2015-05-08 09:56:42 +0200

    Gojira: trapez setters and getters

--- spec/gojira/Tools/TrapezSpec.php
+++ spec/gojira/Tools/TrapezSpec.php
@@ -11,4 +11,16 @@ class TrapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('gojira\Tools\Trapez');
     }
+    
+    function it_should_have_setter_and_getter()
+    {
+        $this->setA(5)->getA()->shouldReturn(5);
+        $this->setB(10)->getB()->shouldReturn(10);
+        $this->setH(2)->getB()->shouldReturn(2);
+    }
+    
+    function it_should_calculate_pole()
+    {
+        $this->setA(5)->setB(10)->$this->setH(2)->poletrojkata()->shouldReturn(25);
+    }
 }
