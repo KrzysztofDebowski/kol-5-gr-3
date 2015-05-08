c2a1358b3ea46ccd8817fce0b26a6e6dfc6da078
Kamil Skowronek <kamil_skowronek@hotmail.com>
2015-05-08 10:03:01 +0200

    Pole Trapezu

--- spec/KamilSkowronek/Tools/TrapezSpec.php
+++ spec/KamilSkowronek/Tools/TrapezSpec.php
@@ -11,4 +11,15 @@ class TrapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('KamilSkowronek\Tools\Trapez');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(345)->getH()->shouldReturn(345);
+    }
+    function it_should_calculate_pole()
+{
+    $this->setA(3)->setB(7)->setH(5)->poletrapezu()->shouldReturn(25);
+}
 }
