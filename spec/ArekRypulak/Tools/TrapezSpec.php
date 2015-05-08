1672461f5a6574be36e3ef45f002643668a9b648
Arek Rypulak <arekrypulak@o2.pl>
2015-05-08 10:02:57 +0200

    Pole trapezu

--- spec/ArekRypulak/Tools/TrapezSpec.php
+++ spec/ArekRypulak/Tools/TrapezSpec.php
@@ -7,8 +7,19 @@ use Prophecy\Argument;
 
 class TrapezSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+     function it_is_initializable()
     {
-        $this->shouldHaveType('ArekRypulak\Tools\Trapez');
+        $this->shouldHaveType('ArekRypulak\Tools\trapez');
+    }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(345)->getH()->shouldReturn(345);
+    }
+    function it_should_calculate_pole()
+    {
+        $this->setA(5)->setB(5)->setH(7)->poletrapezu()->shouldReturn(35);
     }
 }
