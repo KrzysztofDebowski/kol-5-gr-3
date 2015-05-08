0f1ff676c4079619f5005c8dbc3c56860545a165
Kamil Chmielowski <kamil.chmielowski@hotmail.com>
2015-05-08 10:14:21 +0200

    szwej0/trapez : metody .php

--- spec/szwej0/Tools/TrapezSpec.php
+++ spec/szwej0/Tools/TrapezSpec.php
@@ -11,4 +11,16 @@ class TrapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('szwej0\Tools\Trapez');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(347)->getH()->shouldReturn(347);
+
+    }
+    function it_should_calculate_getarea()
+    {
+        $this->setA(5)->setB(4)->setH(6)->pole()->shouldReturn(27);
+    }
 }
