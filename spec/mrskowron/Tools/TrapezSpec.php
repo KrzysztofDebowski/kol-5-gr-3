255590641b14a4b16808fc4ed0c550814b9fa14d
Michal Skowron <mrskowron@student.kul.lublin.pl>
2015-05-08 10:29:11 +0200

    koniec

--- spec/mrskowron/Tools/TrapezSpec.php
+++ spec/mrskowron/Tools/TrapezSpec.php
@@ -10,5 +10,18 @@ class TrapezSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('mrskowron\Tools\Trapez');
+        
+    }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(987)->getH()->shouldReturn(987);
+    }
+    
+     function trapez()
+    {
+        $this->setA(10)->setB(10)->setH(1)->sum()->shouldReturn(10);
     }
 }
--- src/mrskowron/Tools/Trapez.php
+++ src/mrskowron/Tools/Trapez.php
@@ -4,4 +4,45 @@ namespace mrskowron\Tools;
 
 class Trapez
 {
+      private $a;
+      private $b;
+      private $h;
+
+    public function setA($a)
+    {
+         $this->a = $a;
+           return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function setB($b)
+    {
+         $this->b = $b;
+            return $this;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function setH($h)
+    {
+         $this->h = $h;
+            return $this;
+    }
+
+    public function getH()
+    {
+        return $this->h;
+    }
+   
+    public function sum()
+    {
+        return $this->a + $this->b * $this->h /2;
+    }
 }
